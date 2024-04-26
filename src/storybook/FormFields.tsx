b8bf5bf1eb609a952e6c4adad41e71060664adfd
Øivind Viktor Jenssen <oivind.jenssen@webstep.no>
2024-04-26 13:38:36 +0200

    fix: skriv om FormFields til funksjon for å støtte bruk av useScreenSize hook i hjelpemetode for inputfelt

--- src/storybook/FormFields.tsx
+++ src/storybook/FormFields.tsx
@@ -10,30 +10,97 @@ import {
   PageGeneratorSupportedFields,
 } from '../types';
 
-export const FormFields: (PageGeneratorField | PageGeneratorRow)[] = [
-  {
-    component: PageGeneratorSupportedFields.Heading,
-    props: {
-      level: 1,
+export const FormFields = (): (PageGeneratorField | PageGeneratorRow)[] => {
+  return [
+    {
+      component: PageGeneratorSupportedFields.Heading,
+      props: {
+        level: 1,
+      },
+      innerHTML: 'Skjema',
     },
-    innerHTML: 'Skjema',
-  },
-  {
-    component: PageGeneratorSupportedFields.Heading,
-    props: {
-      level: 2,
+    {
+      component: PageGeneratorSupportedFields.Heading,
+      props: {
+        level: 2,
+      },
+      innerHTML: 'Personinformasjon',
     },
-    innerHTML: 'Personinformasjon',
-  },
-  NinInput({
-    fieldProps: {
-      validations: [
-        {
-          message: 'Påkrevd',
-          rule: (value: string) => {
-            return value.length > 0;
+    NinInput({
+      fieldProps: {
+        validations: [
+          {
+            message: 'Påkrevd',
+            rule: (value: string) => {
+              return value.length > 0;
+            },
           },
+          {
+            message: 'Må være omg',
+            rule: (value: string) => {
+              return value === 'omg';
+            },
+          },
+        ],
+      },
+    }),
+    {
+      fields: [
+        DateOfBirthDatepicker(),
+        {
+          component: PageGeneratorSupportedFields.Select,
+          props: {
+            label: 'Status',
+            name: 'status',
+            options: [
+              { label: 'Glad', value: 'glad' },
+              { label: 'Superglad', value: 'superglad' },
+            ],
+          },
+          name: 'status',
         },
+      ],
+    },
+    {
+      component: PageGeneratorSupportedFields.Heading,
+      props: {
+        level: 3,
+      },
+      innerHTML: 'Kontaktinformasjon',
+    },
+    EmailInput({}),
+    PhoneNumberRow(
+      {
+        fieldProps: {
+          validations: [
+            {
+              message: 'Må være +47',
+              rule: (value: string) => {
+                return value === '+47';
+              },
+            },
+          ],
+        },
+      },
+      {
+        fieldProps: {
+          validations: [
+            {
+              message: 'Telefonnummer kan bare inneholde tall',
+              rule: (value: string) =>
+                value.length === 0 || value.match(/^\d+$/) !== null,
+            },
+          ],
+        },
+      },
+    ),
+    {
+      component: PageGeneratorSupportedFields.TextArea,
+      props: {
+        label: 'Adresse',
+        name: 'adresse',
+      },
+      validations: [
         {
           message: 'Må være omg',
           rule: (value: string) => {
@@ -42,158 +109,106 @@ export const FormFields: (PageGeneratorField | PageGeneratorRow)[] = [
         },
       ],
     },
-  }),
-  {
-    fields: [
-      DateOfBirthDatepicker(),
-      {
-        component: PageGeneratorSupportedFields.Select,
-        props: {
-          label: 'Status',
-          name: 'status',
-          options: [
-            { label: 'Glad', value: 'glad' },
-            { label: 'Superglad', value: 'superglad' },
-          ],
-        },
-        name: 'status',
+    {
+      component: PageGeneratorSupportedFields.Heading,
+      props: {
+        level: 4,
       },
-    ],
-  },
-  {
-    component: PageGeneratorSupportedFields.Heading,
-    props: {
-      level: 3,
+      innerHTML: 'Annet',
     },
-    innerHTML: 'Kontaktinformasjon',
-  },
-  EmailInput({}),
-  PhoneNumberRow({
-    fieldProps: {
-      validations: [
+    {
+      component: PageGeneratorSupportedFields.CheckboxGroup,
+      props: {
+        label: 'Kvalifikasjoner',
+      },
+      children: [
         {
-          message: 'Må være +47',
-          rule: (value: string) => {
-            return value === '+47';
+          component: PageGeneratorSupportedFields.Checkbox,
+          props: {
+            label: 'Glad',
+            value: 'glad',
+          },
+        },
+        {
+          component: PageGeneratorSupportedFields.Checkbox,
+          props: {
+            label: 'Superglad',
+            value: 'superglad',
           },
         },
       ],
     },
-  }),
-  {
-    component: PageGeneratorSupportedFields.TextArea,
-    props: {
-      label: 'Adresse',
-      name: 'adresse',
-    },
-    validations: [
-      {
-        message: 'Må være omg',
-        rule: (value: string) => {
-          return value === 'omg';
-        },
+    {
+      component: PageGeneratorSupportedFields.RadioButtonGroup,
+      props: {
+        label: 'Kjønn',
+        name: 'gender',
       },
-    ],
-  },
-  {
-    component: PageGeneratorSupportedFields.Heading,
-    props: {
-      level: 4,
-    },
-    innerHTML: 'Annet',
-  },
-  {
-    component: PageGeneratorSupportedFields.CheckboxGroup,
-    props: {
-      label: 'Kvalifikasjoner',
-    },
-    children: [
-      {
-        component: PageGeneratorSupportedFields.Checkbox,
-        props: {
-          label: 'Glad',
-          value: 'glad',
-        },
-      },
-      {
-        component: PageGeneratorSupportedFields.Checkbox,
-        props: {
-          label: 'Superglad',
-          value: 'superglad',
-        },
-      },
-    ],
-  },
-  {
-    component: PageGeneratorSupportedFields.RadioButtonGroup,
-    props: {
-      label: 'Kjønn',
-      name: 'gender',
-    },
-    children: [
-      {
-        component: PageGeneratorSupportedFields.RadioButton,
-        props: {
-          label: 'Mann',
-          value: 'mann',
-          name: 'gender',
-        },
-      },
-      {
-        component: PageGeneratorSupportedFields.RadioButton,
-        props: {
-          label: 'Kvinne',
-          value: 'kvinne',
-          name: 'gender',
-        },
-      },
-    ],
-  },
-  {
-    component: PageGeneratorSupportedFields.ToggleBar,
-    props: {
-      label: 'Ja eller nei',
-      name: 'yesno',
-    },
-    children: [
-      {
-        component: PageGeneratorSupportedFields.ToggleRadio,
-        props: {
-          label: 'Ja',
-          value: 'yes',
-          name: 'yesno',
-        },
-      },
-      {
-        component: PageGeneratorSupportedFields.ToggleRadio,
-        props: {
-          label: 'Nei',
-          value: 'no',
-          name: 'yesno',
-        },
-      },
-    ],
-  },
-  {
-    fields: [
-      {
-        component: PageGeneratorSupportedFields.Button,
-        props: {
-          label: 'Lagre',
-          type: 'submit',
-        },
-      },
-      {
-        component: PageGeneratorSupportedFields.Button,
-        props: {
-          label: 'Superlagre',
-          purpose: 'secondary',
-          onClick: event => {
-            event.preventDefault();
-            console.log('Du superlagret!');
+      children: [
+        {
+          component: PageGeneratorSupportedFields.RadioButton,
+          props: {
+            label: 'Mann',
+            value: 'mann',
+            name: 'gender',
           },
         },
+        {
+          component: PageGeneratorSupportedFields.RadioButton,
+          props: {
+            label: 'Kvinne',
+            value: 'kvinne',
+            name: 'gender',
+          },
+        },
+      ],
+    },
+    {
+      component: PageGeneratorSupportedFields.ToggleBar,
+      props: {
+        label: 'Ja eller nei',
+        name: 'yesno',
       },
-    ],
-  },
-];
+      children: [
+        {
+          component: PageGeneratorSupportedFields.ToggleRadio,
+          props: {
+            label: 'Ja',
+            value: 'yes',
+            name: 'yesno',
+          },
+        },
+        {
+          component: PageGeneratorSupportedFields.ToggleRadio,
+          props: {
+            label: 'Nei',
+            value: 'no',
+            name: 'yesno',
+          },
+        },
+      ],
+    },
+    {
+      fields: [
+        {
+          component: PageGeneratorSupportedFields.Button,
+          props: {
+            label: 'Lagre',
+            type: 'submit',
+          },
+        },
+        {
+          component: PageGeneratorSupportedFields.Button,
+          props: {
+            label: 'Superlagre',
+            purpose: 'secondary',
+            onClick: event => {
+              event.preventDefault();
+              console.log('Du superlagret!');
+            },
+          },
+        },
+      ],
+    },
+  ];
+};
