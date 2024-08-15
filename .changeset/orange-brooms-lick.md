0bd84896b4991df9cd1e69bf6543a18cf7ee361d
Øivind Jenssen <40202454+oivindjenssen@users.noreply.github.com>
2024-08-15 12:43:53 +0200

    feat(validation): Legg til validatorer for Required, Nin, PhoneNumber og Email (#99)

---
+++ .changeset/orange-brooms-lick.md
@@ -0,0 +1,5 @@
+---
+'@norges-domstoler/dds-page-generator': minor
+---
+
+Legg til validatorer for Required, Nin, PhoneNumber og Email
---
+++ src/helpers/Validators.tsx
@@ -0,0 +1,41 @@
+import { idnr } from '@navikt/fnrvalidator';
+import { PageGeneratorValidation } from '../types';
+
+export const RequiredValidator = (
+  message?: string,
+  formMessage?: string,
+): PageGeneratorValidation => {
+  return {
+    message: message || 'Feltet er påkrevd',
+    formMessage: formMessage || 'Feltet er påkrevd',
+    rule: (value: string) => value.length > 0,
+  };
+};
+
+export const NinValidator: PageGeneratorValidation = {
+  message: 'Fødselsnummer er ugyldig',
+  formMessage: 'Fødselsnummer er ugyldig',
+  rule: (value: string) => {
+    if (value.length > 0) {
+      const result = idnr(value);
+      return result.status === 'valid';
+    }
+    return true;
+  },
+};
+
+export const PhoneNumberValidator: PageGeneratorValidation = {
+  message: 'Kan bare inneholde tall',
+  formMessage: 'Telefonnummer kan bare inneholde tall',
+  rule: (value: string) =>
+    value.length > 0 ? value.match(/^\d+$/) !== null : true,
+};
+
+export const EmailValidator: PageGeneratorValidation = {
+  message: 'E-post er ugyldig',
+  formMessage: 'E-post er ugyldig',
+  rule: (value: string) =>
+    value.length > 0
+      ? value.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/) !== null
+      : true,
+};
